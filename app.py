3418701f2342e51ce461141551d77ec775010511
Shishir Rijal <ccrrizal0892@gmail.com>
2024-09-25 17:09:01 +0545

    add logger and handle exception

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 import streamlit as st
+import logging
 import pickle
 from utils.preprocess import preprocess
 
-tfidf = pickle.load(open('./model/vectorizer.pkl', 'rb'))
-model = pickle.load(open('./model/model.pkl', 'rb'))
 
-st.title("E-mail Spam Classifier")
+logging.basicConfig(level=logging.DEBUG)
 
-input_sms = st.text_input("Enter the message here...")
+try:
+    tfidf = pickle.load(open('./model/vectorizer.pkl', 'rb'))
+    model = pickle.load(open('./model/model.pkl', 'rb'))
 
-if st.button("Predict", type="primary"):
-    # 1. Preprocess
-    cleaned_sms = preprocess(input_sms)
+    st.title("E-mail Spam Classifier")
 
-    # 2. Vectorize
-    vector_input = tfidf.transform([cleaned_sms])
+    input_sms = st.text_input("Enter the message here...")
 
-    # 3. Predict
-    prediction = model.predict(vector_input)[0]
+    if st.button("Predict", type="primary"):
+        # 1. Preprocess
+        cleaned_sms = preprocess(input_sms)
 
-    # 4. Display result
-    if prediction == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+        # 2. Vectorize
+        vector_input = tfidf.transform([cleaned_sms])
+
+        # 3. Predict
+        prediction = model.predict(vector_input)[0]
+
+        # 4. Display result
+        if prediction == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
+except Exception as e:
+    print(f"Exception: {e}")
+
+
+
 
 
 
